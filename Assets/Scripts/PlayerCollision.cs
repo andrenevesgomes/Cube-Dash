37a93101a01618370a70878823ea8f0c72a020f1
André Gomes <agomes@altyra.com>
2021-05-05 00:54:50 +0100

    Obstaculo

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,15 @@
+
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public Player movement; //referencia ao script que move o jogador
+    void OnCollisionEnter(Collision info) //Collider com os objectos que tem RigidBody
+    {
+        if (info.collider.tag == "Obstacle") //se o player colidir com um objecto com a tag "Obstacle" o movimento para
+        {
+            movement.enabled = false;
+        }
+    }
+
+}
