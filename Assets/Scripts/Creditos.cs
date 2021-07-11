b603332d9631673815cee45104fdc1f72ec61a53
André Gomes <agomes@altyra.com>
2021-07-11 17:45:04 +0100

    Added Soundtrack to the proj

--- Assets/Scripts/Creditos.cs
+++ Assets/Scripts/Creditos.cs
@@ -4,7 +4,7 @@ public class Creditos : MonoBehaviour
 {
     public void Sair()
     {
-        Debug.Log("Sai!");
+        Debug.Log("Sai!"); //ver se realmente foi efectuado o comando
         Application.Quit();
     }
 }
